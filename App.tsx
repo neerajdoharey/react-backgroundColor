75545a54edb3af44b50291d6d1446431afe5444a
Neeraj Doharey <neeraj.doharey@live.com>
2025-01-22 18:04:52 +0530

    Add shapes with random color

--- App.tsx
+++ App.tsx
@@ -16,21 +16,33 @@ import {
 } from 'react-native';
 
 function App(): React.JSX.Element {
-  const [newColor, setNewColor] = useState('#ffffff')
+  const [ranbackgroundColor, setranbackgroundColor] = useState('#ffffff')
+  const [rancirleColor, setrancirleColor] = useState('#FF9800')
+  const [ranSquareColor, setranSquareColor] = useState('#FF9800')
 
   const generateColor = () => {
     const hexRange = "0123456789ABCDEF"
-    let color = "#"
-    for (let i = 0; i < 6; i++) {
-      color += hexRange[Math.floor(Math.random() * 16 )]
+    let colors = [];
+    for (let j = 0; j < 3; j++) {
+      let color = "#"
+      for (let i = 0; i < 6; i++) {
+        color += hexRange[Math.floor(Math.random() * 16 )]
+      }
+      colors.push(color)
     }
-    setNewColor(color)
+    setranbackgroundColor(colors[0])
+    setrancirleColor(colors[1])
+    setranSquareColor(colors[2])
   }
 
   return (
     <>
-      <StatusBar backgroundColor={newColor}/>
-      <View style={[styles.container, {backgroundColor: newColor}]}>
+      <StatusBar backgroundColor={ranbackgroundColor}/>
+      <View style={[styles.container, {backgroundColor: ranbackgroundColor}]}>
+        <View style={[styles.circleShape, {backgroundColor: rancirleColor}]} />
+        {/* <View style={[styles.pentagon, {backgroundColor: ranPantagonColor}]} />
+        <View style={[styles.triangleShape, {backgroundColor: ranTriangleColor}]} /> */}
+        <View style={[styles.squareShape, {backgroundColor: ranSquareColor}]} />
         <TouchableOpacity
           onPress={generateColor}
         >
@@ -55,7 +67,22 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 40
   },
-  btnText:{ color: '#ffffff', fontSize: 24}
+  btnText:{ color: '#ffffff', fontSize: 24},
+  circleShape: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,  // Half of width/height
+    position: 'absolute',
+    left: 50,
+    top: 200,
+  },
+  squareShape: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    right: 50,
+    top: 200,
+  }
 });
 
 export default App;
